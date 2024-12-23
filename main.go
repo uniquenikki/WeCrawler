24514e947668369d8f408856236ad5d1fe034cf9
uniquenikki <59661115+uniquenikki@users.noreply.github.com>
2024-12-23 11:38:59 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"webCrawler/webcrawler"
 
 	"github.com/gin-gonic/gin"
@@ -15,9 +16,17 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		webcrawler.WebCrawler(c)
 	})
+	r.GET("/home", func(c *gin.Context) {
+		HomeHandler(c)
+	})
 
 	// Start the server on port 8080
 	if err := r.Run(":8080"); err != nil {
 		panic("Failed to start the server: " + err.Error())
 	}
+}
+func HomeHandler(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "Hi there. This is the Autotron Core API built in Golang!",
+	})
 }
